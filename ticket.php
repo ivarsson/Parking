38bf17e90f0955c722c84200bd815fc462840d9c
Adam Abenius <adam.abenius@d05.se>
2011-12-06 14:49:42 +0100

    Deteled things in  ticket

--- ticket.php
+++ ticket.php
@@ -11,30 +11,7 @@
 
 	<div id="main">
 
-	<a href="#" id="paper" class="mobileTicket" data-role="button">Pappersbiljett <br/><br/><br/> Tryck för att välja</a>
-	
-	<div id="mobileButton">
-		<a href="#" id="mobile" class="mobileTicket" data-role="button">SMS-biljett</a>
-		<div id="smsTicket" class="mobileTicket" style="display:none;">
-			<form action="" method="POST">
-			<input type="text" name="regnr" id="regnr" placeholder="Reg.nummer" />
-			<br />
-			<input type="text" name="telnr" id="telnr" placeholder="Mobilnummer" />
-			<br />
-			<input type="submit" name="cancel" value="Ångra">
-			<input type="submit" name="submit" value="Nästa" />
-			<form>
-		</div>
-	</div>
 
-
-	<div id="smsTicket" style="display:none;">
-		<label for="regnr">Reg.nummer: </label>
-		<input type="text" name="regnr" id="regnr" />
-		<br />
-		<label for="telnr">Mobilnummer: </label>
-		<input type="text" name="telnr" id="telnr" />
-	</div>
 
 </div>
 
