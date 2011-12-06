c09d020290866bf7b60999eaa6d3f0f6c4c5fea9
Johan Ivarsson <johan.ivarsson88@gmail.com>
2011-12-06 15:44:43 +0100

    confirmMobile

---
+++ confirmMobile.php
@@ -0,0 +1,17 @@
+<div data-role="header" data-theme="a">
+	<h1>Bekräfta</h1>
+</div>
+
+<div id="progressBar">
+	<span class="done">1. Välj tjänst</span>
+	<span class="done">2. Välj tid</span>
+	<span class="done">3. Biljettyp</span>
+	<span class="active">4. Bekräfta</span>
+</div>
+
+<div data-role="footer" data-theme="a">
+	<p>
+		<a class="back" onClick="window.location.reload()" href="#ticket" data-role="button" data-inline="true" data-icon="back">Tillbaka</a>
+
+	</p>
+</div>
