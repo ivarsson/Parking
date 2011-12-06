8595775c93d81c26361b792728673f25249fcf9b
Adam Abenius <adam.abenius@d05.se>
2011-12-06 11:25:17 +0100

    Local CSS instead of http ones
    Downloaded images aswell.

--- index.php
+++ index.php
@@ -4,10 +4,10 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	<title>P-automat</title> 
-	<link rel="stylesheet" href="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.css" />
+	<link rel="stylesheet" href="jqueryMobile.css" />
 	<link rel="stylesheet" href="style.css" />
 	<script src="http://code.jquery.com/jquery-1.6.4.min.js"></script>
-	<script src="http://code.jquery.com/mobile/1.0/jquery.mobile-1.0.min.js"></script>
+	<script src="jqueryMobile.js"></script>
 	<script src="http://maps.google.com/maps/api/js?sensor=true" type="text/javascript"></script>
 	<script type="text/javascript" src="maps.js"></script>
 	<script type="text/javascript" charset="utf-8">
