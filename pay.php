d29c62e1ca356a52e0de06da253c0ea9ba5a6451
Adam Abenius <adam.abenius@d05.se>
2011-12-06 12:31:07 +0100

    Styling of pay.php

--- pay.php
+++ pay.php
@@ -11,16 +11,22 @@
 	
 <div data-role="fieldcontain" >
 
-	<label for="timmar">Timmar: </label>
-	<input type="range" name="timmar" id="timmar" value="0" min="0" max="23"  />
-	<br />
-	<label for="minuter">Minuter: </label>
-	<input type="range" name="minuter" id="minuter" value="0" min="0" max="60"  />
-	<br />
-	<br />
-	<label for="slider">Att betala: </label>
-	<input type="text" id="betala" name="betala" />
+	<table id="hoursMinTable">
+		
+	<tr>
+		<td class="headerTd"><label for="timmar">Timmar: </label></td>
+		<td><input type="range" name="timmar" id="timmar" value="0" min="0" max="23"  /></td>
+	</tr>
 	
+	<tr>
+		<td><label for="minuter">Minuter: </label></td>
+		<td><input type="range" name="minuter" id="minuter" value="0" min="0" max="60"  /></td>
+	</tr>
+	<tr>
+		<td><label for="slider">Totalsumma: </label></td>
+		<td><input type="text" id="betala" name="betala" />SEK</td>
+	</tr>
+	</table>
 </div>
 
 
